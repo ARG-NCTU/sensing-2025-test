b2664ab93bd9cc95d183b0835a59334dc4d8b8a0
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2025-02-20 13:57:05 +0800

    fix: update notebook directory path
    Updated the notebook directory path in jupyter_notebook.sh script to
    /home/arg/sensing-2025-test from /home/arg/aoop-2024-mid-term-exam. This
    change ensures that the Jupyter notebook starts in the correct directory.

--- jupyter_notebook.sh
+++ jupyter_notebook.sh
@@ -1,7 +1,7 @@
 jupyter notebook \
     --NotebookApp.allow_origin='https://colab.research.google.com' \
     --NotebookApp.port_retries=0 \
-    --notebook-dir /home/arg/aoop-2024-mid-term-exam \
+    --notebook-dir /home/arg/sensing-2025-test \
     --port=8888 \
     --allow-root \
     --no-browser \
